464ab5bbe3c25eaf69dd4751244980f385240736
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2024-10-26 19:13:09 +0200

    Add License

--- src/MainWindow.pl
+++ src/MainWindow.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/perl
+#  Software: ShellyInsights
+#  File: MainWindow.pl
+#  Copyright: 2024 Thomas Obernosterer
+#  SPDX-License-Identifier: MPL-2.0
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use strict;
 use warnings FATAL => 'all';
 
--- src/lib/ShellySDK/Devices/EM.pm
+++ src/lib/ShellySDK/Devices/EM.pm
@@ -1,3 +1,12 @@
+#  Software: ShellyInsights
+#  File: ShellySDK/Devices/EM.pm
+#  Copyright: 2024 Thomas Obernosterer
+#  SPDX-License-Identifier: MPL-2.0
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 package ShellySDK::Devices::EM;
 use strict;
 use warnings FATAL => 'all';
