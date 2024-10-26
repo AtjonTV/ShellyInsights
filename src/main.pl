df23313d42837115994bebbb7660cd8ebc67d6a8
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2024-10-26 19:01:30 +0200

    Wrap GUI logic into function and make it updated with a timer

--- src/main.pl
+++ src/main.pl
@@ -14,28 +14,31 @@ sub get_status {
 use Prima qw(Application Buttons Label);
 
 my $wnd = Prima::MainWindow->new(
-    text     => 'Shelly Insights v0.0.1',
-    size     => [ 400, 100],
+    text => 'Shelly Insights v0.0.1',
+    size => [ 400, 100 ],
+    skin => "classic"
 );
 
-my $box = $wnd->insert(Widget =>
-    expand => 1,
-    pack => {side => "top"}
-);
+sub mdi_pro_3em {
+    my $box = $wnd->insert(Widget =>
+        expand => 1,
+        pack => {side => "top"}
+    );
 
-my $lbl = $box->insert(Label =>
-    text                     => 'Watts: ???',
-    pack                     => { side => "left" },
-);
+    my $lbl = $box->insert(Label =>
+        text                     => 'Watts: ???',
+        pack                     => { side => "left" },
+    );
 
-$box->insert(Button =>
-    text            => "Click to Refresh",
-    onClick         => sub {
-        my $new_status = ShellySDK::Devices::EM::get_status("10.1.0.90")->{'current_watts'};
-        $new_status = int($new_status);
-        $lbl->set_text("Watts: $new_status");
-    },
-    pack            => { side => "left" },
-);
+    my $timer = Prima::Timer->create(
+        timeout => 500, # milliseconds
+        onTick  => sub {
+            my $new_status = get_status();
+            $lbl->set_text("Watts: $new_status");
+        },
+    );
+    $timer->start();
+}
+mdi_pro_3em();
 
 run Prima;
